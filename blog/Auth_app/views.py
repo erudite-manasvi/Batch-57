cd0d2cddefd95d1a302c6fb2525a22e83f93b7b6
Manasvi Joshi <manasvijoshi63@gmail.com>
2025-07-02 07:01:31 -0400

    setup views in Auth_app

--- blog/Auth_app/views.py
+++ blog/Auth_app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def home_view(request):
+    return HttpResponse('<h1>Hello World</h1>')
+
+def signup(request):
+    return render(request,'Auth_app/index.html')
--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,6 +115,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "Static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
