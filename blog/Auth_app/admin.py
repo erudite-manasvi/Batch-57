3660b14ab2bd6d96ae40e622a002e4fc775b5d0f
Manasvi Joshi <manasvijoshi63@gmail.com>
2025-07-03 13:51:24 -0400

    models

--- blog/Auth_app/admin.py
+++ blog/Auth_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Person
+
 # Register your models here.
+# admin.site.register(Person)
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display= ["name","phone","email"]
--- blog/Auth_app/models.py
+++ blog/Auth_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+    phone=models.CharField(max_length=10)
+    email=models.EmailField(max_length=30,primary_key=True)
