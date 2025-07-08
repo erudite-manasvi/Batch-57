9f6bfafdacbf4aaa9982100ba49c51af111f0630
Manasvi Joshi <manasvijoshi63@gmail.com>
2025-07-08 13:40:21 -0400

    api-practical

--- blog/Auth_app/urls.py
+++ blog/Auth_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('home/',views.home_view),
     path('signup/',views.signup),
-    path('get-data/',views.get_data)
+    path('get-data/',views.get_data),
+    path('get-data-by-id/<str:email>/',views.get_data_by_id)
 ]
--- blog/Auth_app/views.py
+++ blog/Auth_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, HttpResponse
 from .models import Person
 from django.http import JsonResponse
-
+from django.views.decorators.csrf import csrf_exempt
+import json
 from .serializers import PersonSerializer
 
 # Create your views here.
@@ -11,8 +12,79 @@ def home_view(request):
 def signup(request):
     return render(request,'Auth_app/index.html')
 
+@csrf_exempt
 def get_data(request):
     if request.method=='GET':
         data=Person.objects.all()
         serialized_data = PersonSerializer(data,many=True)
         return JsonResponse(serialized_data.data,safe=False)
+    
+    if request.method == 'POST':
+        input_data = request.body # raw JSON(bytes)
+        raw_data = json.loads(input_data) # raw JSON(bytes) -> Python dict
+        sd = PersonSerializer(data=raw_data) #desrialization
+
+        if sd.is_valid():
+            sd.save()
+            return JsonResponse({
+                "success":True,
+                "message":"Data saved successfully"
+            }, status=201)
+        
+        return JsonResponse({
+            "success":False,
+            "message":sd.errors
+        })
+    
+@csrf_exempt
+def get_data_by_id(request,email):
+    
+    data = Person.objects.get(email=email) #finding data from DB
+
+    if request.method=='GET':
+        sd = PersonSerializer(data) #serialization
+        return JsonResponse(sd.data,safe=False)
+    
+    if request.method == 'PUT':
+        input_data=json.loads(request.body)
+
+        sd = PersonSerializer(data,data=input_data)
+
+        if sd.is_valid():
+            sd.save()
+
+            return JsonResponse({
+                "success":True,
+                "message":"Data updated successfully"
+            },status=200)
+        
+        return JsonResponse({
+            "success":False,
+            "message":sd.errors
+        },status=500)
+    
+    if request.method == 'PATCH':
+        input_data=json.loads(request.body)
+
+        sd = PersonSerializer(data,data=input_data,partial=True)
+
+        if sd.is_valid():
+            sd.save()
+
+            return JsonResponse({
+                "success":True,
+                "message":"Data updated successfully"
+            },status=200)
+        
+        return JsonResponse({
+            "success":False,
+            "message":sd.errors
+        },status=500)
+    
+    if request.method == 'DELETE':
+        data.delete()
+
+        return JsonResponse({
+            "success":True,
+            "message":"Data deleted successfully"
+        },status=200)
